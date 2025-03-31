da169c43182d7961927196a68e151c6ae2cfb96c
John Daryll Sampilingan <johndaryllramos8@gmail.com>
2025-03-31 16:28:46 +0800

    change to cup

--- js/script.js
+++ js/script.js
@@ -212,7 +212,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     
     // Initialize total score calculation for all cups
-    for (let i = 1; i <= 10; i++) {
+    for (let i = 1; i <= 5; i++) {
         calculateTotalScore(i);
     }
 });
